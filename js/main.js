aed94b9e05e43f7380c7981e49fc2ff75acee047
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2019-04-11 20:09:30 +0300

    düzenlemeler yapıldı

--- js/main.js
+++ js/main.js
@@ -5,6 +5,36 @@ $(function(){
     });
 
     $('[data-toggle="tooltip"]').tooltip();
+
+    $(".switch").on('click', (e) => {
+        console.log(e.target.id);
+        if(e.target.id == "login"){
+            $("#loginForm").removeClass("d-block");
+            $("#loginForm").addClass("d-none");
+            $("#registerForm").removeClass("d-none");
+            $("#registerForm").addClass("d-block");
+
+        } else if(e.target.id == "register") {
+            $("#loginForm").removeClass("d-none");
+            $("#loginForm").addClass("d-block");
+            $("#registerForm").removeClass("d-block");
+            $("#registerForm").addClass("d-none");
+        }
+        // if($(this).parent().parent().parent().data("login-durum") == 1){
+        //     $("#registerForm").addClass("d-block");
+        //     $("#loginForm").addClass("d-none");
+        //     $(this).parent().parent().parent().data("login-durum", 0);
+        //     console.log($(".switch").parent().parent().parent().data("login-durum"));
+        // } else {
+        //
+        //     $("#loginForm").addClass("d-block");
+        //     $("#registerForm").addClass("d-none");
+        //     $(this).parent().parent().parent().data("login-durum", 1);
+        //     console.log($(".switch").parent().parent().parent().data("login-durum"));
+        // }
+
+    });
+
 });
 
 
